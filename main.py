15364a28cc95b6d99077419fe6779eb9d2bce3b3
ellisk42@gmail.com <ellisk42@gmail.com>
2022-07-18 10:13:55 -0400

    definitely some problems with parsing

--- main.py
+++ main.py
@@ -21,46 +21,52 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 testcases = [
-    ("3af2c5a8", Floating(Sprite())),
-    ("7ddcd7ec", Union(Rectangle(height=None, width=None),
-                       Repeat(Rectangle(height=1, width=1)))),
+    ("6c434453", Repeat(Sprite(color=1))),
+    ("6e82a1ae", Repeat(Sprite(color=5))),
     
-    ("6e82a1ae", Repeat(Sprite(color=5))), 
+    # ("3af2c5a8", Floating(Sprite())),
+    # ("7ddcd7ec", Union(Rectangle(height=None, width=None),
+    #                    Repeat(Rectangle(height=1, width=1)))),
+    
+    
+    
+    
 
-    ("2281f1f4", Repeat(Rectangle(color=5, height=1, width=1))), 
-    ("ded97339", Repeat(Rectangle(color=8, height=1, width=1))),
-    ("5c0a986e", Union(Rectangle(color=1), Rectangle(color=2))),
+    # ("2281f1f4", Repeat(Rectangle(color=5, height=1, width=1))), 
+    # ("ded97339", Repeat(Rectangle(color=8, height=1, width=1))),
+    # ("5c0a986e", Union(Rectangle(color=1), Rectangle(color=2))),
     
     
     
-    ("d631b094", Repeat(Rectangle(height=1, width=1))),
+    # ("d631b094", Repeat(Rectangle(height=1, width=1))),
     
-    ("6150a2bd", Repeat(Rectangle(height=1, width=1))), 
-    ("a87f7484", Repeat(Sprite())),
+    # ("6150a2bd", Repeat(Rectangle(height=1, width=1))),
+    # ("62c24649", Repeat(Rectangle(height=1, width=1))),
     
-    ("025d127b", Repeat(Vertical(Floating(Rectangle()),
-                                        Vertical(Horizontal(Floating(Diagonal()), Diagonal()),
-                                                 Floating(Rectangle()))))), 
+    # ("a87f7484", Repeat(Sprite())),
     
-    ("1caeab9d", Horizontal(Horizontal(Floating(Rectangle()), Floating(Rectangle())),
-                            Floating(Rectangle()))),
-    ("b8cdaf2b", Horizontal(Floating(Rectangle()),
-                            Horizontal(Floating(Vertical(Rectangle(), Rectangle())),
-                                       Floating(Rectangle())))), 
-    ("99b1bc43", Vertical(Sprite(diffuse=True),
-                          Vertical(Rectangle(),
-                                   Sprite(diffuse=True)))),
+    # ("025d127b", Repeat(Vertical(Floating(Rectangle()),
+    #                                     Vertical(Horizontal(Floating(Diagonal()), Diagonal()),
+    #                                              Floating(Rectangle()))))), 
+    
+    # ("1caeab9d", Horizontal(Horizontal(Floating(Rectangle()), Floating(Rectangle())),
+    #                         Floating(Rectangle()))),
+    # ("b8cdaf2b", Horizontal(Floating(Rectangle()),
+    #                         Horizontal(Floating(Vertical(Rectangle(), Rectangle())),
+    #                                    Floating(Rectangle())))), 
+    # ("99b1bc43", Vertical(Sprite(diffuse=True),
+    #                       Vertical(Rectangle(),
+    #                                Sprite(diffuse=True)))),
     
     
-    ("97999447", Repeat(Rectangle(height=1, width=1))),
-    ("a3325580", Repeat(Sprite())),
-    ("a78176bb", Union(Diagonal(),
-                       Repeat(Sprite(color=5, contiguous=True)))),   
+    # ("97999447", Repeat(Rectangle(height=1, width=1))),
+    # ("a3325580", Repeat(Sprite())),
+    # ("a78176bb", Union(Diagonal(),
+    #                    Repeat(Sprite(color=5, contiguous=True)))),   
 
     #          requires overlapping sprites
     # ("e5062a87", Union(Sprite(color=2), Sprite(color=5))),
 
-    # requires smarter union
     ("47c1f68c", Union(Union(Rectangle(height=1), Rectangle(width=1)),
                        Sprite())),
              
@@ -123,6 +129,7 @@ def test_manual_parsers():
     for code, time in sorted(times.items(), key=lambda ct: ct[1]):
         print(code, time)
     print("Total time:", sum(times.values()))
+    print("Total problems:", len(times))
 
     from plotting import plot_arc_array
     for code, parses in parses.items():
